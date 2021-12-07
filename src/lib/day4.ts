4ceb61a44681b8b4c80b47f55c7d8485aa0477cb
adventofcode2021 <95358404+adventofcode2021@users.noreply.github.com>
2021-12-07 22:40:21 +0200

    Solved day 4, part 2
    Implementation for day 4, part 2

--- src/lib/day4.ts
+++ src/lib/day4.ts
@@ -5,10 +5,11 @@ type BingoCell = {
 
 type BingoTable = {
     cells: BingoCell[][];
+    won: boolean;
 };
 
 // Solve Advent of Code 2021
-// Day 3 part 1
+// Day 4 part 1
 export function solveDay4Part1(input: string) : string  {
     const inputs = input.split('\n');
     const numbersString = inputs.shift();
@@ -30,7 +31,7 @@ export function solveDay4Part1(input: string) : string  {
             });
             index++;
             if (index === 5) {
-                allBingoTables.push({ cells: tempBingo });
+                allBingoTables.push({ cells: tempBingo, won: false });
                 tempBingo = [];
                 index = 0;
             }
@@ -69,7 +70,7 @@ export function solveDay4Part1(input: string) : string  {
             for (let i = 0; i < 5; i++) {
                 let allMarked = true;
                 for (let j = 0; j < 5; j++) {
-                    if (!bingo.cells[j][j].marked) {
+                    if (!bingo.cells[j][i].marked) {
                         allMarked = false;
                     }
                 }
@@ -83,7 +84,84 @@ export function solveDay4Part1(input: string) : string  {
 }
 
 // Solve Advent of Code 2021
-// Day 3 part 2
+// Day 4 part 2
 export function solveDay4Part2(input: string) : string  {
+    const inputs = input.split('\n');
+    const numbersString = inputs.shift();
+    if (!numbersString) {
+        return 0 + '';    
+    }
+    const numbers = numbersString.split(',').map(a => +a);
+    let index = 0;
+    let tempBingo: BingoCell[][] = [];
+    const allBingoTables: BingoTable[] = [];
+    inputs.forEach(element => {
+        if (element !== '') {
+            const bingoCells = element.trim().split(/[ ]+/).map(a => +a);
+            bingoCells.forEach(element => {
+                if (!tempBingo[index]) {
+                    tempBingo[index] = [];
+                }
+                tempBingo[index].push({ value: element, marked: false });
+            });
+            index++;
+            if (index === 5) {
+                allBingoTables.push({ cells: tempBingo, won: false });
+                tempBingo = [];
+                index = 0;
+            }
+        }
+    });
+    for (const num of numbers) {
+        for (const bingo of allBingoTables.filter(a => !a.won)) {
+            for(let i = 0; i < 5; i++) {
+                for(let j = 0; j < 5; j++) {
+                    if (bingo.cells[i][j].value === num) {
+                        bingo.cells[i][j].marked = true;
+                    }
+                }
+            }
+            let unmarkedSum = 0;
+            for(let i = 0; i < 5; i++) {
+                for(let j = 0; j < 5; j++) {
+                    if (!bingo.cells[i][j].marked) {
+                        unmarkedSum += bingo.cells[i][j].value;
+                    }
+                }
+            }
+            // Check for horizontal
+            for (let i = 0; i < 5; i++) {
+                let allMarked = true;
+                for (let j = 0; j < 5; j++) {
+                    if (!bingo.cells[i][j].marked) {
+                        allMarked = false;
+                    }
+                }
+                if (allMarked) {
+                    bingo.won = true;
+                    const notWon = allBingoTables.filter(a => !a.won);
+                    if (notWon.length === 0) {
+                        return (num * unmarkedSum) + '';
+                    }
+                }
+            }
+            // Check for vertical
+            for (let i = 0; i < 5; i++) {
+                let allMarked = true;
+                for (let j = 0; j < 5; j++) {
+                    if (!bingo.cells[j][i].marked) {
+                        allMarked = false;
+                    }
+                }
+                if (allMarked) {
+                    bingo.won = true;
+                    const notWon = allBingoTables.filter(a => !a.won);
+                    if (notWon.length === 0) {
+                        return (num * unmarkedSum) + '';
+                    }                    
+                }
+            }
+        }        
+    }
     return 0 + '';
 }
--- tests/unit/adventofcode.spec.ts
+++ tests/unit/adventofcode.spec.ts
@@ -119,6 +119,6 @@ describe('AdventOfCode.vue', () => {
     const solution1 = wrapper.find('#solution1').text();
     const solution2 = wrapper.find('#solution2').text();
     expect(solution1).toMatch('4512');
-    expect(solution2).toMatch('0');
+    expect(solution2).toMatch('1924');
   })  
 })
